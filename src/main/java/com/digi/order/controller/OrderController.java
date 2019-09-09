0955f7ad4de170dd1015ad07a240c8ddd2c72fe5
amitaucs <amit.datta2@wipro.com>
2019-09-09 16:18:29 +0530

    config server added

--- src/main/java/com/digi/order/controller/OrderController.java
+++ src/main/java/com/digi/order/controller/OrderController.java
@@ -7,6 +7,7 @@ import com.digi.order.service.RestaurantOrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/order")
 @CrossOrigin(value="*")
 @Slf4j
+@RefreshScope
 public class OrderController {
 
     @Value("${message:Hello default}")
