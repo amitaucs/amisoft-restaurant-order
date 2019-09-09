d7f03c77b87a3141510be658789142e0ac8ac6b7
amitaucs <amit.datta2@wipro.com>
2019-09-09 16:07:33 +0530

    version update

--- src/main/java/com/digi/order/controller/OrderController.java
+++ src/main/java/com/digi/order/controller/OrderController.java
@@ -6,6 +6,7 @@ import com.digi.order.model.OrderResponse;
 import com.digi.order.service.RestaurantOrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,9 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(value="*")
 @Slf4j
 public class OrderController {
+
+    @Value("${message:Hello default}")
+    private String message;
 
     @Autowired
     RestaurantOrderService restaurantOrderService;
@@ -42,11 +46,11 @@ public class OrderController {
     public ResponseEntity<OrderResponse> getOrder(@RequestParam(value="consumerId") String consumerId) {
 
         log.info("CustomerId is :"+consumerId);
-        return new ResponseEntity<>(new OrderResponse("From Order"),HttpStatus.OK);
+        return new ResponseEntity<>(new OrderResponse("From Order :"+message),HttpStatus.OK);
     }
 
     @GetMapping("/getAllOrders")
     public ResponseEntity<OrderResponse> getOrder(){
-        return new ResponseEntity<>(new OrderResponse("From Order"),HttpStatus.OK);
+        return new ResponseEntity<>(new OrderResponse("From Order :"+message),HttpStatus.OK);
     }
 }
